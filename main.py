051ccfa2631836a128cc1bd5802ec72b8a4da6d6
WiseBoiii <aleksashin.1001@gmail.com>
2025-01-23 19:00:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import os
+import dotenv
+
+url = 'https://xkcd.com/info.0.json'
+comic_channel_id = os.getenv('CHAT_ID')
+
+
+def download_comic_image(comic_image_link):
+    filename = 'comic'
+    comic_image = requests.get(comic_image_link)
+    comic_image.raise_for_status()
+    with open(f'{filename}.png', 'wb') as file:
+        file.write(comic_image.content)
+    print(funny_comment)
+
+
+response = requests.get(url)
+response.raise_for_status()
+all_about_comic = response.json()
+comic_image_link = all_about_comic['img']
+funny_comment = all_about_comic['alt']
+download_comic_image(comic_image_link)
